1b058ac42b21d93d1310914e57d6beaf8d387b2c
JBendici <josemanuel.bendici01@estudiant.upf.edu>
2024-10-20 16:37:43 +0200

    Update antiprime.py

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,7 +1,7 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-def main() :
+def main(n) :
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
 
@@ -11,7 +11,26 @@ def main() :
 	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
 	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
 	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
+	d = 0
+	i = 1
+	while i <= n:
+		if n % i == 0:
+			d = d + 1
+		i = i + 1
+	return d
+
+def ap(n):
+	dn = main(n)
+	i = 1
+	while i < n:
+		if cd(i) >= dn:
+			return("Not anti-prime
+   		i = i + 1
+	return("Anti-prime")
+
+ numero = int(sys,argv[1])
+
+ap(numero)
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
@@ -21,4 +40,5 @@ if __name__ == "__main__" :
 	## $ python antiprime.py 6
 	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
 	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
+	$ python antiprime.py 6
 	print(main())
