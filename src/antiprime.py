ca3e7c23f0d2baae49f629b8441a3ee46d744981
JBendici <josemanuel.bendici01@estudiant.upf.edu>
2024-10-20 17:02:05 +0200

    Update antiprime.py

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,36 +1,31 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-def main(n) :
-	## YOU CODE SHOULD START HERE AST THE SAME
-	## IDENTATION AS THIS COMMENT
+import sys
 
+def main(n):
+    d = 0
+    i = 1
+    while i <= n:
+        if n % i == 0:
+            divisores += 1
+        i += 1
+    return divisores
 
-	## THE LAST LINES OF YOUR CODE SHOULD EITHER
-	## RETURN THE VALUE "anti-prime" or "not anti-prime"
-	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
-	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
-	## "anti-prime" or "not anti-prime"
-	d = 0
-	i = 1
-	while i <= n:
-		if n % i == 0:
-			d = d + 1
-		i = i + 1
-	return d
+def es_antiprime(n):
+    num_divisores = main(n)
+    i = 1
+    while i < n:
+        if main(i) >= num_divisores:
+            return "not anti-prime"  # Retornamos "not anti-prime" si encontramos un número menor con igual o más divisores
+        i += 1
+    return "antiprime"  
 
-def ap(n):
-	dn = main(n)
-	i = 1
-	while i < n:
-		if cd(i) >= dn:
-			return("Not anti-prime")
-		i = i + 1
-	return("Anti-prime")
+numero = int(sys.argv[1])
 
-numero = int(sys,argv[1])
+resultado = es_antiprime(numero)
+print(resultado)
 
-ap(numero)
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
